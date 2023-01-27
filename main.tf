d0a31c6292fc3a41848c80e56f88c739ce04593f
ganesha87 <ganeshvarma.cse@gmail.com>
2023-01-27 15:06:28 +0530

    Testing using existing permissionset

--- main.tf
+++ main.tf
@@ -43,10 +43,17 @@ module "sso_account_assignments" {
       principal_type      = "GROUP",
       principal_name      = "Developers"
     },
-      {
+    {
       account             = "116309360350",
       permission_set_arn  = module.permission_sets.permission_sets["Engg-ReadOnly"].arn,
       permission_set_name = "Engg-ReadOnly",
+      principal_type      = "GROUP",
+      principal_name      = "Testers"
+    },
+    {
+      account             = "262169116715", # Represents the "Sandbox" account
+      permission_set_arn  = "arn:aws:sso:::permissionSet/ssoins-65958a9bd17fab05/ps-23bdfb80e564879e",
+      permission_set_name = "AWSReadOnlyAccess", #Adding an existing PermissionSet
       principal_type      = "GROUP",
       principal_name      = "Testers"
     }
