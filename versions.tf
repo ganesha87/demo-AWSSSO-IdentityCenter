bef7618e32710035847ac499d3673a779a906fc9
ganesha87 <ganeshvarma.cse@gmail.com>
2023-01-27 12:21:43 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,3 +1,11 @@
 terraform {
   required_version = ">= 0.13.0"
+  backend "remote" {
+  # The name of your Terraform Cloud organization.    
+  organization = "ganesha87"
+    
+  # The name of the Terraform Cloud workspace to store Terraform state files in.
+  workspaces {
+    name = "Demo-AWSSSO-IdentityCenter"
+  }
 }
